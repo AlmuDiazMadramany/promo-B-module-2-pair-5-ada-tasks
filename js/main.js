94cde33cbb42319e7baaa97be13da810b0ea7cea
Claudia <claurodher2@gmail.com>
2025-01-08 11:38:38 +0100

    checkbox and filter tasks

--- js/main.js
+++ js/main.js
@@ -6,18 +6,65 @@ const tasks = [
   ];
 
 
-  const taskList = document.querySelector (".js-tasklist")
+const taskList = document.querySelector (".js-tasklist")
+const searchBtn = document.querySelector('.js-btn-filter');
+const searchInput = document.querySelector('.js-text-task-filter');
 
 function renderTask(){
         taskList.innerHTML="";
-        for (const eachTask of tasks) {
-                
-                if (eachTask.completed === true){
-                taskList.innerHTML += `<li class ="tachado"> ${eachTask.name} </li>`
-                } else {
-                    taskList.innerHTML += `<li> ${eachTask.name} </li>`
-                }
-            }
-        }
 
-renderTask ();
+        for (const eachTask of tasks) {
+            const isChecked = eachTask.completed ? "checked" : ""; 
+            const isTachado = eachTask.completed ? "tachado" : "";
+
+            taskList.innerHTML += ` <li class="${isTachado}"> <input type="checkbox" id="task-${eachTask.id}" ${isChecked} data-id="${eachTask.id}"> <label for="task-${eachTask.id}">${eachTask.name}</label> </li>`;
+
+            //     if (eachTask.completed === true){
+            //         taskList.innerHTML += `<li class ="tachado"><input type="checkbox" id="${eachTask.id}"> ${eachTask.name} </li>`
+            //     } else {
+            //         taskList.innerHTML += `<li><input type="checkbox"> ${eachTask.name} </li>`
+            //     }
+             }
+
+        addCheckboxListeners(); 
+    
+    }
+
+// Añadir eventos a los checkboxes después de renderizar 
+
+function addCheckboxListeners() {
+const checkboxes = document.querySelectorAll("input[type='checkbox']"); 
+checkboxes.forEach((checkbox) => { 
+    checkbox.addEventListener("change", (event) => { 
+        const taskId = parseInt(event.target.dataset.id); 
+
+        for (let i = 0; i < tasks.length; i++) { 
+            if (tasks[i].id === taskId) { 
+                tasks[i].completed = event.target.checked;
+                break;
+            } 
+        } 
+        renderTask();
+
+    }); 
+}); 
+
+} 
+
+
+renderSearch(list) {
+    for (const element of list) {
+        taskList.innerHTML += `<li> ${element.name} </li>`
+    }
+}
+
+
+function handleChange () {
+    const searchValue = searchInput.value;
+    const filterTasks = tasks.filter((task) => task.name.toLowerCase() === searchValue);
+    renderSearch(filterTasks);
+}
+
+renderTask();
+    
+searchBtn.addEventListener('change', handleChange);
