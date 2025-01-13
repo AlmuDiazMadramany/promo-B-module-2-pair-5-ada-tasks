c1199998d0139ed06bd3862137b433f502292680
AlmuDiazMadramany <almumadra@hotmail.com>
2025-01-13 11:30:38 +0100

    click cheked

--- js/main.js
+++ js/main.js
@@ -11,6 +11,18 @@ let tasks = [];
 const GITHUB_USER = "AlmuDiazMadramany";
 const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
 
+// FUNCION PARA PINTAR LAS TAREAS EN EL HTML
+const renderTasks = () => {
+    taskList.innerHTML = ""; // Limpiar la lista actual
+    for (const task of tasks) {
+      if (task.completed) {
+        taskList.innerHTML += `<li class="tachado"><input type="checkbox" checked id="${task.id}">${task.name}</li>`;
+      } else {
+        taskList.innerHTML += `<li><input type="checkbox" id="${task.id}">${task.name}</li>`;
+      }
+    }
+  };
+
 
 // TAREAS PINTADAS EN HTML DESDE EL SERVIDOR
 fetch (SERVER_URL)
@@ -18,34 +30,32 @@ fetch (SERVER_URL)
 .then((data) =>{
     console.log (data)
     tasks=data.results;
-    taskList.innerHTML = "";
-    for (const task of tasks) {
-        if(task.completed){
-            taskList.innerHTML += `<li class="tachado"><input type="checkbox" checked id="${task.id}">${task.name}</li>`
-        } else {
-            taskList.innerHTML += `<li><input type="checkbox" id="${task.id}">${task.name}</li>`
-        }
-    }
+    renderTasks(); // nos traemos la funcion de las tareas pintadas en el HTML
 })
 
 
+// BOTON CLICK EN EL LISTADO
 const handleClickList = (event) => {
-    const taskId = parseInt(event.target.id); 
+    const taskId = parseInt(event.target.id);  // Obtenemos el id del checkbos clicado
     if (!taskId) return; 
-
-    for (const task of tasks) {
-
-    }
-    
-  
-    // Busca la tarea que tenga el id `taskId` en el array `tasks`. O sea ver que numero de id corresponde al checkbox que se ha marcado, y a esa tarea añadirle completed.
-    // Una vez que has obtenido la tarea, actualiza la propiedad `completed`
-    // Pinta de nuevo las tareas en el html
+     const task = tasks.find ((t) => t.id === taskId); // Busca la tarea en el array
+     if (task){
+        task.completed =!task.completed; // Esto cambia el estado a completed, que se actualiza desde el rendertask el estado de checkbox - checked y tachado del texto. 
+        renderTasks (); // Aquí volvemos a pintar las tareas según si esta checked o no. 
+     }
   };
   
-  list.addEventListener("click", handleClickList);
+taskList.addEventListener("click", handleClickList);
 
-taskList.addEventListener('click', handleClickList);
+
+// --> FILTRAR TAREAS -- NOS QUEDAMOS AQUÍ
+const handleSearchBtn =(ev) =>{
+    ev.preventDefault (); // para que no refresque la página
+    const searchValue = searchInput.value; // obtenemos el value y lo pasamos todo a texto con minusculas. 
+    const filterTasks = tasks.filter ((task) => task.name.includes (searchValue)) // Filtra las tareas que tengan solo el texto mostrado y pintamos solo esas tareas: 
+    renderTasks (filterTasks);
+}
+searchBtn.addEventListener ("click", handleSearchBtn);
 
 function newTaskAdded(){
     const taskAdded = {
